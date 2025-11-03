efa53d2dd9bd4672be15718003f2e429eafaef09
federus1105 <federusrudi@gmail.com>
2025-11-03 13:58:42 +0700

    fix : deleting table migrations due to order error

--- 000009_product_categories.up.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE product_categories (
-	id_product int4 NOT NULL,
-	id_categories int4 NOT NULL,
-	CONSTRAINT product_categories_id_categories_fkey FOREIGN KEY (id_categories) REFERENCES categories(id),
-	CONSTRAINT product_categories_id_product_fkey FOREIGN KEY (id_product) REFERENCES product(id)
-);
