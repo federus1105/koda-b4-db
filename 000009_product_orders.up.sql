aea59404e39ca85bb6801bdf4e5206ab61d63b89
federus1105 <federusrudi@gmail.com>
2025-11-03 13:40:35 +0700

    fix: delete table migrations because wrong order

--- 000009_product_orders.up.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE product_orders (
-	id_product int4 NOT NULL,
-	id_order int4 NOT NULL,
-	CONSTRAINT product_orders_id_order_fkey FOREIGN KEY (id_order) REFERENCES orders(id),
-	CONSTRAINT product_orders_id_product_fkey FOREIGN KEY (id_product) REFERENCES product(id)
-);
